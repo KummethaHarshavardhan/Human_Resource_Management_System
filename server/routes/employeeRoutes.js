364a73350387a7e4d7da7d691c44355bec75698c
Sivavempuluru14 <sivavempuluru12@gmail.com>
2026-08-12 12:27:36 +0530

    Modifiled EmployeeRoutes

--- server/routes/employeeRoutes.js
+++ server/routes/employeeRoutes.js
@@ -25,42 +25,42 @@ router.put("/profile", verifyToken, updateMyProfile);
 router.post(
   "/",
   verifyToken,
-  authorizeRoles("Admin", "HR"),
+  authorizeRoles("Admin", "HR", "HR Manager"),
   createEmployee
 );
 
 router.get(
   "/",
   verifyToken,
-  authorizeRoles("Admin", "HR", "Employee"),
+  authorizeRoles("Admin", "HR", "HR Manager"),
   getAllEmployees
 );
 
 router.get(
   "/:id",
   verifyToken,
-  authorizeRoles("Admin", "HR", "Employee"),
+  authorizeRoles("Admin", "HR", "HR Manager"),
   getEmployeeById
 );
 
 router.put(
   "/:id",
   verifyToken,
-  authorizeRoles("Admin", "HR"),
+  authorizeRoles("Admin", "HR", "HR Manager"),
   updateEmployee
 );
 
 router.delete(
   "/:id",
   verifyToken,
-  authorizeRoles("Admin", "HR"),
+  authorizeRoles("Admin", "HR", "HR Manager"),
   deleteEmployee
 );
 
 router.patch(
   "/:id/status",
   verifyToken,
-  authorizeRoles("Admin", "HR"),
+  authorizeRoles("Admin", "HR", "HR Manager"),
   updateEmployeeStatus
 );
 
